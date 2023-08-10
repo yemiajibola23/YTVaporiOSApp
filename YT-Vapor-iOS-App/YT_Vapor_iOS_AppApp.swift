cf9bd31702cdd546c4853d857fa61b6ce5ad1e6a
Yemi Ajibola <yajibola23@gmail.com>
2023-08-10 17:18:48 -0500

    Initial Commit

---
+++ YT-Vapor-iOS-App/YT_Vapor_iOS_AppApp.swift
@@ -0,0 +1,17 @@
+//
+//  YT_Vapor_iOS_AppApp.swift
+//  YT-Vapor-iOS-App
+//
+//  Created by Yemi Ajibola on 8/10/23.
+//
+
+import SwiftUI
+
+@main
+struct YT_Vapor_iOS_AppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ YT-Vapor-iOS-AppUITests/YT_Vapor_iOS_AppUITestsLaunchTests.swift
@@ -0,0 +1,32 @@
+//
+//  YT_Vapor_iOS_AppUITestsLaunchTests.swift
+//  YT-Vapor-iOS-AppUITests
+//
+//  Created by Yemi Ajibola on 8/10/23.
+//
+
+import XCTest
+
+final class YT_Vapor_iOS_AppUITestsLaunchTests: XCTestCase {
+
+    override class var runsForEachTargetApplicationUIConfiguration: Bool {
+        true
+    }
+
+    override func setUpWithError() throws {
+        continueAfterFailure = false
+    }
+
+    func testLaunch() throws {
+        let app = XCUIApplication()
+        app.launch()
+
+        // Insert steps here to perform after app launch but before taking a screenshot,
+        // such as logging into a test account or navigating somewhere in the app
+
+        let attachment = XCTAttachment(screenshot: app.screenshot())
+        attachment.name = "Launch Screen"
+        attachment.lifetime = .keepAlways
+        add(attachment)
+    }
+}
